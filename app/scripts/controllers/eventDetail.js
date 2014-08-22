32a8858a2c596dc0df037ec3c026b5da653d7ad1
Ivan Skrypka <vanish.com90@gmail.com>
2014-08-22 18:10:12 +0300

    make events speakers module visible

--- app/scripts/controllers/eventDetail.js
+++ app/scripts/controllers/eventDetail.js
@@ -7,14 +7,9 @@ angular.module('itytApp').controller('EventDetailCtrl', ['$scope', 'Page', 'Cons
    * 1. Make a proper errors handling
    * 2. Remove events injection - should be in main data at once
    */
-  var title = [Constants.meta.SITE_NAME, data && !data.error && data.name || 'Ошибка'],
-    //To be removed:
-    newSpeakersData = speakers.filter(function(elem) {
-      return data.speakers.indexOf(elem._id) != -1;
-    });
+  var title = [Constants.meta.SITE_NAME, data && !data.error && data.name || 'Ошибка'];
 
   Page.setTitle(title.join(' - '));
-  data.speakers = newSpeakersData;
   $scope.event = data;
 
 }]);
